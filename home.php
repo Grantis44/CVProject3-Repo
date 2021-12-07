65f22922d46d80323f90feb3d9ac12cfbd265e32
Grantis44 <granthughc@gmail.com>
2021-12-07 14:00:26 +0200

    commit

--- home.php
+++ home.php
@@ -1,28 +1,37 @@
-
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Homescreen</title>
-    <link href="home.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Dropdown Menu with Search Box | CodingNepal</title>
+  <link rel="stylesheet" href="home.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
 <body>
-    <div class="full-page">
-        <div class="sub-page">
-            <div class="navigation-bar">
-                <div class="logo">
-                    <a href='home.php'>Home Page</a>
-                </div>
-                <nav>
-                    <ul id='MenuItems'>
-                        <li><a href='index.php'>About Me</a></li>
-                        <li><a href='indexLoading.php'>Downlaod Text CV</a></li>
-                        <li><a href='indexr.php'>Delete</a></li>
-                        <li><a href='index1.php'>Closing Summary</a></li>
-                    </ul>
-                </nav>
-            </div>
-        
+  <div class="wrapper">
+    <nav>
+      <input type="checkbox" id="show-search">
+      <input type="checkbox" id="show-menu">
+      <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
+      <div class="content">
+      <div class="logo"><a href="#">Grant Cunningham CV</a></div>
+        <ul class="links">
+          <li><a href="index.php">Technical Skills</a></li>
+          <li><a href="indexr.php">View Text based CV</a></li>
+          <li><a href="index1.php">About Me</a></li>
+        </ul>
+      </div>
+      <label for="show-search" class="search-icon"><i class="fas fa-search"></i></label>
+      <form action="#" class="search-box">
+        <input type="text" placeholder="Type Something to Search..." required>
+        <button type="submit" class="go-icon"><i class="fas fa-long-arrow-alt-right"></i></button>
+      </form>
+    </nav>
+  </div>
+  <div class="dummy-text">
+    <h2>This is my digital CV</h2>
+    <h2>For More Details Click on a tab above!</h2>
+  </div>
 </body>
 </html>
